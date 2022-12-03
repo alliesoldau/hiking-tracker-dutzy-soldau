ad775dd59d4144bd9e3358708e2a0fe2f3c8b454
Kevin Dutzy <kddutz15@gmail.com>
2022-12-03 17:03:37 -0500

    added form inputs basic

--- src/Components/UserInputForm.js
+++ src/Components/UserInputForm.js
@@ -3,21 +3,28 @@ import React from 'react';
 function UserInputForm() {
     return(
         <div className="UserInputForm">
-            <p>User Input Form</p>
-            <div className="UserInputDropDowns-Container">
-                <div className="Date-Container">
-                    <p>Date</p>
+            <form>
+                <p>User Input Form</p>
+                <div className="UserInputDropDowns-Container">
+                    <div className="Date-Container">
+                        <label for="date" >Date: </label><br/><br/>
+                        <input type="date" id="date" name="date" />
+                    </div>
+                    <div className="Buddies-Container">
+                        <p>Buddies</p>
+                    </div>
+                    <div className="Notes-Container">
+                        <p>Notes</p>
+                    </div>
+                    <div className="Pics-Container">
+                        <label for="myfile">Upload Your Photos: </label><br/>
+                        <input type="file" id="photos" name="photos"/>
+                    </div>
+                    <div>
+                     <button type='submit'>Submit Form</button>
+                    </div>
                 </div>
-                <div className="Buddies-Container">
-                    <p>Buddies</p>
-                </div>
-                <div className="Notes-Container">
-                    <p>Notes</p>
-                </div>
-                <div className="Pics-Container">
-                    <p>Pics</p>
-                </div>
-            </div>
+            </form>
         </div>
     )
 }
