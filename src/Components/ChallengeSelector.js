3ab33efc6abe91bd3813315d1ce7848d622c32f6
Allie Soldau <alliesoldau@gmail.com>
2022-12-05 10:17:10 -0500

    database completed and user input form updated to be more clear

--- src/Components/ChallengeSelector.js
+++ src/Components/ChallengeSelector.js
@@ -1,22 +1,29 @@
 import React from 'react';
-
 function ChallengeSelector() {
-
-    
-
     return(
         <div className="ChallengeSelector">
             <p>Challenge Selector</p>
             <div className="Selector-Container">
                 <div className="Challenges-Container">
-                    <p>Challenges Drop Down</p>
+                    <label>Select a Challenge:</label><br/>
+                    <select  placeholder='Select Challenge...' name='challenges' id='challenges'>
+                        <option>ADK46</option>
+                        <option>Catskill 3500</option>
+                        <option>LG 12</option>
+                        <option>LP 9</option>
+                        <option>Saranac 6</option>
+                    </select>
                 </div>
                 <div className="Mountain-Container">
-                    <p>Mountain Drop Down</p>
+                    <label>Select a Mountain:</label><br/>
+                    <select  placeholder='Select Mountain...' name='mountains' id='mountains'>
+                        <option></option>
+                        {/* we should add a new component to map through the mountains to create option tags -
+                        instead of individually creating 70+ option tags for this  */}
+                    </select>
                 </div>
             </div>
         </div>
     )
 }
-
 export default ChallengeSelector;
--- src/Components/UserInputForm.js
+++ src/Components/UserInputForm.js
@@ -1,34 +1,33 @@
 import React from 'react';
-
 function UserInputForm() {
     return(
         <div className="UserInputForm">
             <form>
-                <p>User Input Form</p>
-                <div>
-                     <button type='submit'>Submit Form</button>
-                </div><br/>
+                <span>User Input Form</span>
                 <div className="UserInputDropDowns-Container">
                     <div className="Date-Container">
-                        <label>Date: </label><br/><br/>
+                        <label>Date: </label><br/>
                         <input type="date" id="date" name="date" />
                     </div>
                     <div className="Buddies-Container">
-                        <label>Buddies: </label>
-                        <input type="text" id="buddies"  name="buddies" />
+                        <label>Buddies: </label><br/>
+                        <input placeholder='Who did you hike with?..'  type="text" id="buddies"  name="buddies" />
                     </div>
                     <div className="Notes-Container">
-                        <label>Notes: </label>
-                        <input type="text" id="notes"  name="notes" />
+                        <label>Notes: </label><br/>
+                        <textarea placeholder='Tell us about your hike..' id="notes"  name="notes" rows="4" cols="55">
+                        </textarea>
                     </div>
                     <div className="Pics-Container">
-                        <label>Upload Your Photos: </label><br/>
-                        <input type="file" id="photos" name="photos"/>
+                        <label>Upload A Photo: </label><br/>
+                        <input type="file" id="photo" name="photo"/>
+                    </div>
+                    <div>
+                     <button type='submit'>Submit Form</button>
                     </div>
                 </div>
             </form>
         </div>
     )
 }
-
 export default UserInputForm;
