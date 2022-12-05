ee4473b92390c15ade7c24a041d376876eb0cbe4
Allie Soldau <alliesoldau@gmail.com>
2022-12-05 14:48:49 -0500

    grabbed data about the individual mountain

--- src/Components/ChallengeSelector.js
+++ src/Components/ChallengeSelector.js
@@ -4,10 +4,17 @@ import MountainsDD from './MountainsDD';
 
 function ChallengeSelector({ rawData }) {
 
+    // TO DO: get ride of selectedChallenge if I end up not needing it
     const [selectedChallenge, setSelectedChallenge] = useState("")
     const [mountainsArray, setMountainsArray] = useState([])
+    const [name, setName] = useState("")
+    const [elevation, setElevation] = useState(0)
+    const [ascent, setAscent] = useState(0)
+    const [length, setLength] = useState(0)
+    const [typicalTime, setTypicalTime] = useState(0)
+    const [difficulty, setDifficulty] = useState(0)
 
-    // TO DO: Find a way to not hard code this
+    // TO DO: Find a way to not hard code the array indexes or get them more systematically
     const indexArray = {
         "ADK46": 0,
         "Catskill 3500": 1,
@@ -28,6 +35,7 @@ function ChallengeSelector({ rawData }) {
     function handleChallengeSelection(e) {
         e.preventDefault()
         const index = indexArray[`${e.target.value}`]
+        // TO DO: get ride of selectedChallenge if I end up not needing it
         setSelectedChallenge(e.target.value)
         setMountainsArray(rawData[index].mountains)
     }
@@ -41,7 +49,28 @@ function ChallengeSelector({ rawData }) {
         )
     })
 
+    function handleMountainSelection(e) {
+        e.preventDefault()
+        const mountain = e.target.value
+        const mountainInfo = mountainsArray.map((mount) => {
+            if (mount.name === mountain) {
+                setName(mount.name)
+                setElevation(mount.elevation)
+                setAscent(mount.ascent)
+                setLength(mount.length)
+                setTypicalTime(mount.typicalTime)
+                setDifficulty(mount.difficulty)
+            }
+        })
+    }    
+    console.log(`name: ${name}`)            
+    console.log(`elevation: ${elevation}`)
+    console.log(`ascent: ${ascent}`)
+    console.log(`length: ${length}`)
+    console.log(`typicalTime: ${typicalTime}`)
+    console.log(`difficulty: ${difficulty}`)
 
+    
 
     return(
         <div className="ChallengeSelector">
@@ -49,7 +78,8 @@ function ChallengeSelector({ rawData }) {
             <div className="Selector-Container">
                 <div className="Challenges-Container">
                     <label>Select a Challenge:</label><br/>
-                    <select onChange={handleChallengeSelection}
+                    <select 
+                        onChange={handleChallengeSelection}
                         placeholder='Select Challenge...' 
                         name='challenge' 
                         id='challenge'>
@@ -58,14 +88,22 @@ function ChallengeSelector({ rawData }) {
                 </div>
                 <div className="Mountain-Container">
                     <label>Select a Mountain:</label><br/>
-                    <select  placeholder='Select Mountain...' name='mountain' id='mountain'>
-                        {mountainsDD}
-                        {/* we should add a new component to map through the mountains to create option tags -
-                        instead of individually creating 70+ option tags for this  */}
+                    <select 
+                        onChange={handleMountainSelection}
+                        placeholder='Select Mountain...' 
+                        name='mountain' 
+                        id='mountain'>
+                            {mountainsDD}
                     </select>
+                    <p>Elevation: </p>
+                    <p>Ascent: </p>
+                    <p>Length: </p>
+                    <p>Time: </p>
+                    <p>Difficulty: </p>
                 </div>
             </div>
         </div>
+        
     )
 }
 export default ChallengeSelector;
